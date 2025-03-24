8c7983f62b8ae556eb9446ee2bf1220ddeaa21ee
chifo <federpena22@gmail.com>
2025-03-24 15:15:00 -0300

    verce

--- frontend/src/utils/requestApi.ts
+++ frontend/src/utils/requestApi.ts
@@ -1,7 +1,7 @@
 const BASE_URL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:1234/api/v1'
-    : 'https://file-reader-nu.vercel.app/api/v1'
+    : 'https://file-reader-ten.vercel.app/api/v1'
 const POST_FILE_URL = `${BASE_URL}/upload-file`
 const GET_MIME_TYPES_URL = `${BASE_URL}/get-mime-types`
 

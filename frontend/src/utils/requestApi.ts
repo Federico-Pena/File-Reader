377894415f2d60e7aa3ee1f47afce8389c00a648
Chifo <federpena22@gmail.com>
2025-04-25 21:01:51 -0300

    Add only python extract

--- frontend/src/utils/requestApi.ts
+++ frontend/src/utils/requestApi.ts
@@ -1,7 +1,4 @@
-const BASE_URL =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:1234/'
-    : 'https://file-reader-again.vercel.app/'
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:1234/' : '/'
 const POST_FILE_URL = `${BASE_URL}api/v1/upload-file`
 const GET_MIME_TYPES_URL = `${BASE_URL}api/v1/get-mime-types`
 
